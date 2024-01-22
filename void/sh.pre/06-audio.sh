276e14cb002463a812eb566c318c58786cde05a6
awbtux <andywb707@gmail.com>
2024-01-21 19:27:15 -0800

    add audio-related stuff

---
+++ void/sh.pre/06-audio.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+case "${audio_setup:-$(chmenu "Which audio interface do you want to use?" "ALSA" "PipeWire" "PulseAudio" "sndio" "none")}" in
+    [aA][lL][sS][aA])
+        export PACKAGES="${PACKAGES:+$PACKAGES }alsa-utils"
+        export SERVICES="${SERVICES:+$SERVICES }alsa"
+        ;;
+    [pP][iI][pP][eE][wW][iI][rR][eE]|[pP][iI][pP][eE])
+        export PACKAGES="${PACKAGES:+$PACKAGES }wireplumber pipewire pipewire-pulse"
+        export SERVICES="${SERVICES:+$SERVICES }"
+        ;;
+    [pP][uU][lL][sS][eE][aA][uU][dD][iI][oO]|[pP][uU][lL][sS][eE])
+        export PACKAGES="${PCAKAGES:+$PACKAGES }piulseaudio alsa-plugins-pulseaudio"
+        ;;
+    [sS][nN][dD][iI][oO])
+        export PACKAGES="${PACKAGES:+$PACKAGES }sndio aucatctl"
+        export SERVICES="${SERVICES:+$SERVICES }sndiod"
+        ;;
+    *) printf "${0##*/}: error: invalid operand $audio_setup to option audio_setup\n"; exit 1 ;;
+esac
